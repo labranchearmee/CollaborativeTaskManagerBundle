c0e393bbb793fa7c010a4f5e917ff65bd194dd3d
bbergstorm <bbergstorm@gmail.com>
2012-02-05 21:34:24 +0100

    add task entity

---
+++ Entity/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brickstorm\CollaborativeTaskManagerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Brickstorm\CollaborativeTaskManagerBundle\Entity\Task
+ */
+class Task
+{
+
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+}
